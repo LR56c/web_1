b711a322d0d80080051f927eaae8952eab661b09
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-03-27 18:23:53 -0300

    drawer html puro

---
+++ src/components/cart_item.js
@@ -0,0 +1,60 @@
+<div
+  class="relative flex flex-col justify-center items-center gap-2 w-4/5 xs:max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow">
+  <div
+    class="flex w-full flex-col xxs:flex-row justify-center items-center">
+    <img
+      src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+      class="h-20 w-20 bg-gray-700 basis-1/2 object-contain rounded-lg"
+      alt="..."/>
+    <div
+      class="flex w-full basis-1/2 justify-center items-center">nombre
+    </div>
+  </div>
+  <div
+    class="flex w-full xxs:flex-row flex-col justify-center items-center">
+    <div
+      class="w-full h-full flex flex-col items-center">
+      <div class="text-gray-400 line-through">$100</div>
+      <div class="flex items-center flex-col gap-2">
+        <div class="text-red-600 text-2xl font-bold">$60</div>
+        <div
+          class="text-red-600 text-xs xs:text-sm rounded-full bg-red-100">-40%
+          descuento
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full h-full flex flex-col justify-center items-center">
+      <p class="font-bold my-2">Cantidad</p>
+      <div
+        class="w-full flex xxs:flex-row flex-col justify-center items-center gap-4">
+        <div
+          class="h-10 border-2 rounded-full flex justify-center items-center">
+          <div class="flex gap-2 px-2">
+            <button>
+              <svg class="h-6 w-6" fill="none" stroke="currentColor"
+                   stroke-width="1.5" viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg"
+                   aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M18 12H6"></path>
+
+              </svg>
+            </button>
+            <p class="mx-2">0</p>
+            <button>
+              <svg class="h-6 w-6" fill="none" stroke="currentColor"
+                   stroke-width="1.5" viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg"
+                   aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M12 6v12m6-6H6"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="absolute right-2 top-2">delete</button>
+</div>
