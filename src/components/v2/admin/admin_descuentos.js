4b1045bddf9e6b224a94d116a0d9d4dafaf862d8
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-03-29 19:56:37 -0300

    v2 admin usuarios y fix tab descuentos

--- src/components/v2/admin/admin_descuentos.js
+++ src/components/v2/admin/admin_descuentos.js
@@ -600,12 +600,15 @@
   <ul
     class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 justify-center items-center">
     <li class="mr-2">
-      <a href="/admin/usuarios"
-         class="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active group">
-        <svg class="w-5 h-5 mr-2 text-blue-600" fill="none"
-             stroke="currentColor" stroke-width="1.5"
-             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-             aria-hidden="true">
+      <a href="/src/pages/admin_usuarios.html"
+         class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group"
+      >
+        <svg
+          fill="none"
+          class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500"
+          stroke="currentColor" stroke-width="1.5"
+          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round"
                 d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path>
         </svg>
@@ -613,13 +616,14 @@
       </a>
     </li>
     <li class="mr-2">
-      <a href="/admin/descuentos"
-         class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group"
+      <a href="/src/pages/admin_descuentos.html"
+         class="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active group"
          aria-current="page">
-        <svg class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500"
-             fill="none" stroke="currentColor" stroke-width="1.5"
-             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-             aria-hidden="true">
+        <svg
+          class="w-5 h-5 mr-2 text-blue-600"
+          fill="none" stroke="currentColor" stroke-width="1.5"
+          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round"
                 d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"></path>
           <path stroke-linecap="round" stroke-linejoin="round"
@@ -629,7 +633,7 @@
       </a>
     </li>
     <li class="mr-2">
-      <a href="/admin/productos"
+      <a href="/src/pages/admin_productos.html"
          class="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group">
         <svg class="w-5 h-5 mr-2 text-gray-400 group-hover:text-gray-500"
              fill="none" stroke="currentColor" stroke-width="1.5"
@@ -643,6 +647,7 @@
     </li>
   </ul>
 </div>
+
 <div class="flex justify-center items-center p-4">
   <button type="button"
           data-modal-target="add-modal"
