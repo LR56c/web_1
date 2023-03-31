d88b392a6919c72af0af3ff0d7d73df41a952964
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-03-30 21:50:38 -0300

    fix color footer, fix nav logo, fix product, fix recomended product, fix product detail

--- src/components/v2/ui/on_sale_product.js
+++ src/components/v2/ui/on_sale_product.js
@@ -1,39 +1,41 @@
 <div class="h-52 w-52">
-  <div
-    class="h-52 w-52 flex flex-col rounded-lg border border-gray-200 bg-white shadow">
-    <div class="h-full w-full basis-2/3">
-      <div
-        class="uk-inline-clip uk-transition-toggle uk-light">
+    <div
+        class="h-52 w-52 flex flex-col rounded-lg border border-gray-200 bg-white shadow">
+        <div class="h-full w-full basis-2/3">
+            <div
+                class="w-full uk-inline-clip uk-transition-toggle uk-light">
               <span
-                class="absolute top-2 right-0 mr-2 rounded-lg bg-red-600 p-2 text-sm font-medium text-white">
+                  class="absolute top-2 right-0 mr-2 rounded-lg bg-red-600 p-2 text-sm font-medium text-white">
                 -40%
               </span>
-        <img
-          class="h-full w-full rounded-t-lg object-contain"
-          src="https://images.unsplash.com/photo-1679215805559-3f8000e4b6ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-          alt=""/>
-        <div class="uk-position-center">
-          <a href="#">
-            <button
-              class="rounded-lg bg-yellow-400 px-5 text-center text-sm font-medium text-white uk-transition-fade py-1">
+                <div class="flex justify-center items-center">
+                    <img
+                        class="rounded-t-lg max-h-32 object-contain"
+                        src="/imagens/chalapuchu.png"
+                        alt=""/>
+                </div>
+                <div class="uk-position-center">
+                    <a href="#">
+                        <button
+                            class="rounded-lg bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br px-5 text-center text-sm font-medium text-white uk-transition-fade py-1">
 
-              Agregar al carrito
-            </button>
-          </a>
+                            Agregar al carrito
+                        </button>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="mx-auto basis-1/3">
-      <div
-        class="my-2 w-full font-medium">
-        <a href="#">Nombre del producto</a>
-      </div>
-      <div class="flex w-full items-center gap-2">
-        <div class="text-gray-400 line-through">$100</div>
-        <div class="text-2xl font-bold text-red-600">
-          $60
+        <div class="mx-auto basis-1/3">
+            <div
+                class="my-2 w-full font-medium">
+                <a href="#">Nombre del producto</a>
+            </div>
+            <div class="flex w-full items-center gap-2">
+                <div class="text-gray-400 line-through">$100</div>
+                <div class="text-2xl font-bold text-red-600">
+                    $60
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
