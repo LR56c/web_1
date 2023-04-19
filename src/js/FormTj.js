c449cfc9bbe31a111639b40763bbfac4eace0661
Cristopher Enrique Piña Lillo <criss6121topher@gmail.com>
2023-04-18 21:20:22 -0400

    JS tarjeta
    falta validacion de campos

---
+++ src/js/FormTj.js
@@ -0,0 +1,38 @@
+
+
+let nombre = $("#nombre");
+nombre.on("input", function( event ) {
+console.log(`Me llego ${nombre.val()}`)});
+
+let apellido = $("#apellido");
+apellido.on("input", function( event ) {
+console.log(`Me llego ${apellido.val()}`)});
+
+let banco = $("#banco");
+banco.on("input", function( event ) {
+console.log(`Me llego ${banco.val()}`)});
+
+let ntarjeta = $("#ntarjeta");
+ntarjeta.on("input", function( event ) {
+console.log(`Me llego ${ntarjeta.val()}`)});
+
+let mes = $("#mes");
+mes.on("input", function( event ) {
+console.log(`Me llego ${mes.val()}`)});
+
+let ano = $("#ano");
+ano.on("input", function( event ) {
+console.log(`Me llego ${ano.val()}`)});
+
+let clavei = $("#clavei");
+clavei.on("input", function( event ) {
+console.log(`Me llego ${clavei.val()}`)});
+
+
+ 
+// function tarjeta(nombre,apellido,banco,nume){
+
+
+
+// }  
+  
