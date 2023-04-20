e8a3f878e7609df4690b1bf43ca58bd86e408ba3
Cristopher Enrique Piña Lillo <criss6121topher@gmail.com>
2023-04-19 21:30:07 -0400

    Formulario tarjeta
    casi listini

--- src/js/FormTj.js
+++ src/js/FormTj.js
@@ -1,38 +1,90 @@
+// let nombre = $("#nombre");
+// nombre.on("input", function (event) {
+//     console.log(`Me llego ${nombre.val()}`)
+// });
+
+// let apellido = $("#apellido");
+// apellido.on("input", function (event) {
+//     console.log(`Me llego ${apellido.val()}`)
+// });
+
+// let banco = $("#banco");
+// banco.on("input", function (event) {
+//     console.log(`Me llego ${banco.val()}`)
+// });
+
+// let ntarjeta = $("#ntarjeta");
+// ntarjeta.on("input", function (event) {
+//     console.log(`Me llego ${ntarjeta.val()}`)
+// });
+
+// let mes = $("#mes");
+// mes.on("input", function (event) {
+//     console.log(`Me llego ${mes.val()}`)
+// });
+
+// let ano = $("#ano");
+// ano.on("input", function (event) {
+//     console.log(`Me llego ${ano.val()}`)
+// });
+
+// let clavei = $("#clavei");
+// clavei.on("input", function (event) {
+//     console.log(`Me llego ${clavei.val()}`)
+// });
 
 
-let nombre = $("#nombre");
-nombre.on("input", function( event ) {
-console.log(`Me llego ${nombre.val()}`)});
+$(document).ready(function () {
+    let nombre = $("#nombre");
+    let apellido = $("#apellido");
+    let banco = $("#banco");
+    let tarjeta = $("#tarjeta");
+    let mes = $("#mes");
+    let ano = $("#ano");
+    let clave = $("#clave");
 
-let apellido = $("#apellido");
-apellido.on("input", function( event ) {
-console.log(`Me llego ${apellido.val()}`)});
+    $("#formulario1").validate({
 
-let banco = $("#banco");
-banco.on("input", function( event ) {
-console.log(`Me llego ${banco.val()}`)});
+        rules: {
+            nombre: { required: true,minlength: 1, maxlength: 50 },
 
-let ntarjeta = $("#ntarjeta");
-ntarjeta.on("input", function( event ) {
-console.log(`Me llego ${ntarjeta.val()}`)});
+            apellido: { required: true, minlength: 1, maxlength: 50 },
 
-let mes = $("#mes");
-mes.on("input", function( event ) {
-console.log(`Me llego ${mes.val()}`)});
+            banco:{required: true, minlength: 1, maxlength: 50},
+            
+            tarjeta: { required: true, minlength: 8, maxlength: 8},
 
-let ano = $("#ano");
-ano.on("input", function( event ) {
-console.log(`Me llego ${ano.val()}`)});
+            mes: { required: true, minlength: 2, maxlength:2},
 
-let clavei = $("#clavei");
-clavei.on("input", function( event ) {
-console.log(`Me llego ${clavei.val()}`)});
+            ano: { required: true, minlength: 2, maxlength:2},
+
+            clave: { required: true, minlength: 10, maxlength: 10}
+
+        },
+        messages:
+        {
+            nombre: "Ingrese una  nombre valido.",
+            apellido: "Ingrese una apellido valido.",
+            banco:"Ingresa un banco pailon.",
+            tarjeta: "La tarjeta debe de llevar 8 digitos.",
+            mes: "Solo numeros del 01 al 12.",
+            ano: "Solo los dos ultimos digitos del año.",
+            clave: "La clave es de 10 digitos!."
+        }
 
 
- 
-// function tarjeta(nombre,apellido,banco,nume){
+    })
 
+    $("#guardar1").click(function () {
+        if ($("#formulario1").valid() == false) {
 
-
-// }  
-  
+            Swal.fire({
+                title: 'Error!',
+                text: 'Vuelve a internarlo',
+                icon: 'error',
+                confirmButtonText: 'Cerrar'
+            })
+            return;
+        }
+    })
+})
